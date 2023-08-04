04a9dd1106a8349571e84851a9e0f4cbee199836
Ezequiel Suarez <ezagus.suarez@gmail.com>
2023-08-04 10:28:07 -0300

    Programa inicial

---
+++ main/main.ino
@@ -0,0 +1,15 @@
+#include <Adafruit_CircuitPlayground.h>
+
+
+
+//#include <Adafruit_LiquidCrystal.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
